2b4e2a9121df9d3c92446cc0d29d835bfc5478c3
rselph <rselph@tibco.com>
2016-01-19 14:17:59 -0800

    Server logic in place, but doesn't execute command

--- portList.go
+++ portList.go
@@ -6,39 +6,43 @@ import (
 	"crypto"
 	_ "crypto/sha256"
 	"encoding/binary"
+	"reflect"
 	"time"
 )
 
 type PortList struct {
+	newList        []int
 	current        []int
 	previous       []int
 	secret         *[]byte
 	sequenceLength int
 	mask           int
 	hi, lo         int
+	testSequence   []int
 }
 
 func newPortList(secret *[]byte, sequenceLength, hi, lo int) *PortList {
-	retval := &PortList{
+	p := &PortList{
 		secret:         secret,
 		sequenceLength: sequenceLength,
 		hi:             hi,
 		lo:             lo,
 	}
+	p.testSequence = make([]int, sequenceLength)
 
 	mask := 1
 	for mask <= (hi - lo) {
 		mask = mask << 1
 	}
 	mask -= 1
-	retval.mask = mask
+	p.mask = mask
 
-	return retval
+	return p
 }
 
 func (p *PortList) update(t time.Time) {
 	epochTime := t.Unix() / int64(refreshInterval.Seconds())
-	p.current = make([]int, p.sequenceLength)
+	p.newList = make([]int, p.sequenceLength)
 
 	hasher := crypto.SHA256.New()
 
@@ -52,16 +56,20 @@ func (p *PortList) update(t time.Time) {
 	master := hasher.Sum(nil)
 
 	n := int64(0)
-	for i := range p.current {
-		p.current[i], n = nextPort(&master, n, p)
+	for i := range p.newList {
+		p.newList[i], n = nextPort(&master, n, p)
 	}
+
+	p.previous = p.current
+	p.current = p.newList
+	p.newList = nil
 }
 
 func nextPort(master *[]byte, n int64, p *PortList) (int, int64) {
 	hasher := crypto.SHA256.New()
 
 	port := p.hi
-	for port >= p.hi {
+	for shouldRejectPort(port, p) {
 		hasher.Reset()
 		hasher.Write(*master)
 		binary.Write(hasher, binary.LittleEndian, n)
@@ -78,4 +86,35 @@ func nextPort(master *[]byte, n int64, p *PortList) (int, int64) {
 	}
 
 	return port, n
+}
+
+func shouldRejectPort(port int, p *PortList) bool {
+	if port < p.lo || port >= p.hi {
+		return true
+	}
+
+	for _, oldPort := range p.newList {
+		if port == oldPort {
+			return true
+		}
+	}
+
+	for _, oldPort := range p.current {
+		if port == oldPort {
+			return true
+		}
+	}
+
+	for _, oldPort := range p.previous {
+		if port == oldPort {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (p *PortList) checkFull(port int) bool {
+	p.testSequence = append(p.testSequence[1:], port)
+	return reflect.DeepEqual(p.current, p.testSequence) || reflect.DeepEqual(p.previous, p.testSequence)
 }
--- server.go
+++ server.go
@@ -10,31 +10,45 @@ import (
 
 func runServer(s *[]byte) {
 	ports := newPortList(s, knockSequenceLength, portRangeHigh, portRangeLow)
-	startTime := time.Now()
-	ports.update(startTime.Add(-refreshInterval))
-	ports.update(startTime)
 
 	wg := &sync.WaitGroup{}
 	events := make(chan int)
 	var currentListeners, previousListeners []net.Listener
 
+	updateListenState := func(now time.Time) error {
+		ports.update(now)
+		fmt.Println(ports.current)
+		newListeners, err := openPorts(ports.current, events, wg)
+		if err != nil {
+			return err
+		}
+		closePorts(previousListeners)
+		previousListeners = currentListeners
+		currentListeners = newListeners
+		return nil
+	}
+
+	startTime := time.Now()
+	updateListenState(startTime.Add(-refreshInterval))
+	updateListenState(startTime)
+
 	ticktock := time.Tick(refreshInterval)
+
 	for true {
 		select {
 		case now := <-ticktock:
-			ports.update(now)
-			newListeners, err := openPorts(ports.current, events, wg)
+			err := updateListenState(now)
 			if err != nil {
 				fmt.Println("Open ports failed.")
 				fmt.Println(err)
-				continue
 			}
-			closePorts(previousListeners)
-			previousListeners = currentListeners
-			currentListeners = newListeners
 
 		case port := <-events:
-			fmt.Printf("<%v\n", port)
+			fmt.Printf(">%v\n", port)
+			if ports.checkFull(port) {
+				// do server stuff!!
+				fmt.Println("KNOCK SUCCEEDED")
+			}
 		}
 	}
 }
